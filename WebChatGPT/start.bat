3aa31afa0390d625cff12d2a73b36f68375f11af
WangHao <1046911101@qq.com>
2023-03-19 00:01:36 +0800

    fixed windows bat

--- WebChatGPT/start.bat
+++ WebChatGPT/start.bat
@@ -1,18 +1,25 @@
 @echo off
 
 rem 切换到服务端目录，启动服务并将输出写入日志文件
-cd C:\Users\WangHao\Sites\学习\JavaScript\FunnyProject\WebChatGPT\service
-start cmd /c "pnpm start > service.log & echo Start service complete!"
+cd E:\Study\JavaScript\FunnyProject\WebChatGPT\service
+start /B cmd /c "pnpm start > service.log"
+echo Start service complete!
+
+REM 等待 3 秒钟，确保服务已经启动完成 
+timeout /t 3
 
 rem 切换到前端代码目录，清空日志文件，启动服务并将输出写入日志文件
-cd C:\Users\WangHao\Sites\学习\JavaScript\FunnyProject\WebChatGPT
+cd E:\Study\JavaScript\FunnyProject\WebChatGPT
 echo. > front.log
-start cmd /c "pnpm dev > front.log & echo Start front complete!"
-
-rem 等待3秒，确保服务已经启动
-ping localhost -n 3 > nul
+start /B cmd /c "pnpm dev >> front.log 2>&1"
+echo Start front complete!
 
 rem 输出前端服务日志文件内容
 type front.log
 
+rem 等待3秒，确保服务已经启动 
+ping localhost -n 3 > nul
+
 pause
+
+taskkill /F /im node.exe
