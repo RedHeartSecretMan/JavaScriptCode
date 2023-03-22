a0f5e1df302794bcbd5d75b5370539341e9e173c
WangHao <1046911101@qq.com>
2023-03-22 21:41:02 +0800

    fixed avatar

--- WebChatGPT/src/store/modules/user/helper.ts
+++ WebChatGPT/src/store/modules/user/helper.ts
@@ -15,7 +15,7 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: "https://raw.githubusercontent.com/RedHeartSecretMan/go_chatgpt/main/assets/headphoto.jpeg",
+      avatar: "https://raw.githubusercontent.com/RedHeartSecretMan/JavaScriptProject/main/WebChatGPT/src/assets/avatar.jpeg",
       name: 'WangHao',
       description: "What Why How",
     },
